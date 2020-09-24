2fc58f84ae5f082b7aea54caeaa4c04abffab82f
Tochigi4649 <y.muraoka.168@nitech.jp>
2020-09-24 16:51:24 +0900

    32bitまでなら読める

--- Sample/RMD-X8_readAngle/RMD-X8_readAngle.ino
+++ Sample/RMD-X8_readAngle/RMD-X8_readAngle.ino
@@ -23,40 +23,77 @@ float euler_z;
 2:左モータ
 */
 const uint16_t MOTOR_ADDRESS_1 = 0x141; //0x140 + ID(1~32)
+const uint16_t MOTOR_ADDRESS_2 = 0x142; //0x140 + ID(1~32)
 const int SPI_CS_PIN = 9;
 
 MCP_CAN CAN(SPI_CS_PIN); //set CS PIN
-SoftwareSerial MCSerial(8, 9); // RX, TX
 
 RMDx8Arduino rmd1(CAN, MOTOR_ADDRESS_1);
+RMDx8Arduino rmd2(CAN, MOTOR_ADDRESS_2);
 
 void setup()
 {
   SERIAL.begin(115200);
   while (!Serial) ;
-
-  MCSerial.begin(57600);
   
   rmd1.canSetup();
+  rmd2.canSetup();
   delay(1000);
 
   rmd1.clearState();
+  rmd2.clearState();
   rmd1.writePID(40, 40, 50, 40, 10, 20);
+  rmd2.writePID(40, 40, 50, 40, 10, 20);
   delay(1000);
 
   rmd1.serialWriteTerminator();
   timer[0] = millis();
 }
 
+uint32_t angle;
+int32_t angle1, angle2;
+
 void loop()
 {
+  rmd1.writeCurrent(0);
+  rmd2.writeCurrent(0);
   rmd1.readAngle(1);
-  long long angle =rmd1.reply_buf[3] << 24 + rmd1.reply_buf[2] << 16 + rmd1.reply_buf[1] << 8 + rmd1.reply_buf[0];
-
+  rmd2.readAngle(1);
+  angle = ((uint32_t)rmd1.pos_buf[4] << 24) + ((uint32_t)rmd1.pos_buf[3] << 16) + ((uint32_t)rmd1.pos_buf[2] << 8) + rmd1.pos_buf[1];
   if (angle > 2147483648)
   {
-    angle = angle - 4294967296;
+    angle1 = angle - 4294967296;
+  }
+  else
+  {
+    angle1 = angle;
+  }
+
+  
+  angle = ((uint32_t)rmd2.pos_buf[4] << 24) + ((uint32_t)rmd2.pos_buf[3] << 16) + ((uint32_t)rmd2.pos_buf[2] << 8) + rmd2.pos_buf[1];
+  if (angle > 2147483648)
+  {
+    angle2 = angle - 4294967296;
+  }
+  else
+  {
+    angle2 = angle;
   }
   
-  SERIAL.print(angle);
+   SERIAL.print(angle1/600);
+   SERIAL.print(",");
+   SERIAL.print(angle2/600);
+   SERIAL.println("");
+
+
+//   SERIAL.print(rmd1.pos_buf[0]);
+//   SERIAL.print(",");
+//   SERIAL.print(rmd1.pos_buf[1]);
+//   SERIAL.print(",");
+//   SERIAL.print((unsigned long)rmd1.pos_buf[2] << 8);
+//   SERIAL.print(",");
+//   SERIAL.print((unsigned long)rmd1.pos_buf[3] << 16);
+//   SERIAL.print(",");
+//   SERIAL.print((unsigned long)rmd1.pos_buf[4] << 24);
+//   SERIAL.println("");
 }
