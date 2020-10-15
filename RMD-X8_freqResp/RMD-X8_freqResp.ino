4beed81eeb02c548912d88d32a7fddc8c64f699e
bump5236 <t.sanada.330@nitech.jp>
2020-10-15 11:40:03 +0900

    freqResp

---
+++ RMD-X8_freqResp/RMD-X8_freqResp.ino
@@ -0,0 +1,86 @@
+#include <mcp_can.h>
+#include <SPI.h>
+#include <RMDx8Arduino.h>
+
+/*SAMD core*/
+#ifdef ARDUINO_SAMD_VARIANT_COMPLIANCE
+#define SERIAL serialUSB
+#else
+#define SERIAL Serial
+#endif
+
+
+unsigned long timer[3];
+bool exit_tf = false;
+float ang_1[2];
+int16_t tgt;
+int A = 100;
+float f = 0.1;
+
+const uint16_t MOTOR_ADDRESS_1 = 0x144; //0x140 + ID(1~32)
+const int SPI_CS_PIN = 9;
+
+MCP_CAN CAN(SPI_CS_PIN); //set CS PIN
+
+RMDx8Arduino rmd1(CAN, MOTOR_ADDRESS_1);
+
+void setup()
+{
+  SERIAL.begin(115200);
+  while (!Serial) ;
+  
+  rmd1.canSetup();
+  delay(1000);
+
+  rmd1.clearState();
+  
+  rmd1.writePID(40, 40, 50, 40, 20, 20);
+  delay(1000);
+
+  rmd1.serialWriteTerminator();
+  rmd1.readPosition();
+  ang_1[0] = rmd1.present_position / 600; // 基準の角度 [deg]
+}
+
+void loop()
+{
+  for (int i = 1; i <= 8; i++)
+  {
+    timer[0] = millis();
+    timer[1] = 0;
+    while (timer[1] < 5000)
+    {
+      timer[1] = millis() - timer[0];
+  
+      rmd1.readPosition();
+      ang_1[1] = rmd1.present_position / 600 - ang_1[0]; // モータ角度 [deg]
+      
+      tgt = A * sin(2 * 3.14 * f * i * timer[1] * 0.001);
+      
+      rmd1.writeCurrent(tgt);
+    
+      // SerialCommunication ---------------------
+      SERIAL.print(timer[1]);
+      SERIAL.print(",");
+      SERIAL.print(i);
+      SERIAL.print(",");
+      SERIAL.print(tgt);
+      SERIAL.print(",");
+      SERIAL.print(rmd1.present_current);
+      SERIAL.print(",");
+      SERIAL.print(rmd1.present_velocity);
+      SERIAL.print(",");
+      SERIAL.println(ang_1[1]);
+      
+      rmd1.serialWriteTerminator();
+      // ------------------------------------------
+    }
+  }
+  rmd1.clearState();
+  delay(500);
+  SERIAL.println("Program finish!");
+  while (true)
+  {
+    delay(100);
+  }
+}
---
+++ RMD-X8_freqResp/serial2csv.py
@@ -0,0 +1,89 @@
+import serial
+import time
+import csv
+import os 
+import re
+
+
+def save(file_name, head, data):
+    # normal save
+    f = open(file_name,'w')
+    csvWriter = csv.writer(f, lineterminator='\n')
+    csvWriter.writerows(head)
+    csvWriter.writerows(data)
+    f.close()
+    print("--- Saved ---")
+
+def file_count():
+    dir = os.getcwd()
+    files = os.listdir(dir)
+    count = 0
+    for file in files:
+        index = re.search('.csv', file)
+        if index:
+            count = count +1
+    return count
+
+class motor_data:
+    def __init__(self):
+        self.temperature = 0
+        # self.add_cur = 0
+        self.tgt_cur = 0
+        self.current = 0
+        self.velocity = 0
+        self.angle = 0
+
+m1 = motor_data()
+
+ser =serial.Serial("/dev/ttyACM0", 115200)
+time.sleep(2)
+
+for _ in range(20):
+    c = ser.readline()
+
+org_data = []
+save_data = []
+val = []
+timer = [0, 0, 0]
+
+try:
+    timer[0] = time.time()
+
+    while True:
+        timer[1] = time.time()
+        c = ser.readline()
+        c = c.strip().decode('utf-8')
+        org_data.append(c.split(","))
+
+        if len(org_data[-1]) == 6:
+            val = org_data[-1]
+
+            arduino_time = val[0]
+
+            i_n = val[1]
+            m1.tgt_cur = val[2]
+            m1.current = val[3]
+            m1.velocity = val[4]
+            m1.angle = val[5]
+
+            m1_values = [v for v in m1.__dict__.values()]
+
+            l = [timer[1]-timer[0], arduino_time, i_n]
+            l.extend(m1_values)
+            save_data.append(l)
+
+            print(save_data[-1])
+
+except KeyboardInterrupt:
+    ser.close()
+
+    m1_keys = ["M1_"+ k for k in m1.__dict__.keys()]
+
+    h = ["TIME", "ARDUINO_TIME", "i"]
+    h.extend(m1_keys)
+
+    header = [h]
+
+    num = file_count()
+    save("cur_kp20_ki20" + str(num) + ".csv", header, save_data)
+    print("--- Fin ---")
