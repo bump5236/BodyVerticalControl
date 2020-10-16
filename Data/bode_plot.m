e0cc875e4d983ac66f328bbb34209b09cbaa8083
bump5236 <t.sanada.330@nitech.jp>
2020-10-16 10:53:22 +0900

    ボード線図作成用プログラム

--- Data/bode_plot.m
+++ Data/bode_plot.m
@@ -9,10 +9,11 @@ set(0, 'defaultTextFontSize', 16);
 set(0, 'defaultTextFontName', 'times');
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-addpath(pwd, 'FrequencyResponse');
-file_list = ["pos_kp40_ki40_kd50_PRO.csv", "pos_kp80_ki40_kd50_PRO.csv", "pos_kp120_ki40_kd50_PRO.csv"];
-freqset = [0.1 0.2, 0.4, 0.8, 1.6, 3.2, 6.4, 12.8];
-c_set = ['b', 'g', 'r'];
+addpath(pwd, 'FrequencyResponse\10.15.2');
+file_list = ["pos_kp20_ki40_kd50_4.csv", "pos_kp40_ki40_kd50_3.csv", "pos_kp80_ki40_kd50_2.csv", "pos_kp120_ki40_kd50_1.csv", "pos_kp160_ki40_kd50_5.csv"];
+% file_list = ["pos_kp20_ki40_kd50_PRO6.csv", "pos_kp40_ki40_kd50_PRO7.csv", "pos_kp80_ki40_kd50_PRO8.csv", "pos_kp120_ki40_kd50_PRO9.csv", "pos_kp160_ki40_kd50_PRO10.csv"];
+freqset = [0.1 0.2, 0.4, 0.8, 1.6, 3.2, 6.4, 12.8, 25.6];
+c_set = ['b', 'g', 'r', 'c', 'k'];
 
 
 for i_n = 1:length(file_list)
@@ -27,19 +28,28 @@ for i_n = 1:length(file_list)
 %     figure
     
     
-    for i = 1:8
+    for i = 1:9
         X0 = find(num == i - 1, 1);
         X = find(num == i, 1);
-        
         bode_data(i).inputAmp = max(input(X0+10 : X - 1));
         bode_data(i).outputAmp = max(output(X0+10 : X - 1));
+        
+        if i == 9
+            bode_data(i).inputAmp = max(input(X0+10 : end));
+            bode_data(i).outputAmp = max(output(X0+10 : end));
+        end
+        
         list(i) = bode_data(i).outputAmp/bode_data(i).inputAmp;
 
         semilogx(freqset(i), list(i), 'ko')
         hold on
     end
     
-    plot(freqset, list, c_set(i_n))
+%     plot(freqset, list, c_set(i_n))
+    p = polyfit(freqset, list, 2);
+    x = linspace(0,30);
+    y = polyval(p, x);
+    plot(x, y)
     xlabel("frequency [Hz]")
     ylabel("magnitude [dB]")
 
