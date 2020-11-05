952e44d63ccbb0bb1ed6d7204e3db2ec24257df2
bump5236 <t.sanada.330@nitech.jp>
2020-11-05 18:10:14 +0900

    電流値のボード線図を描いてみる

--- Data/bode_plot.m
+++ Data/bode_plot.m
@@ -9,14 +9,15 @@ set(0, 'defaultTextFontSize', 20);
 set(0, 'defaultTextFontName', 'times');
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-addpath(pwd, 'FrequencyResponse\10.19');
+addpath(pwd, 'FrequencyResponse\10.26');
 % file_list = ["pos_kp20_ki40_kd50_4.csv", "pos_kp40_ki40_kd50_3.csv", "pos_kp80_ki40_kd50_2.csv", "pos_kp120_ki40_kd50_1.csv", "pos_kp160_ki40_kd50_5.csv"];
 % file_list = ["pos_kp20_ki40_kd50_PRO6.csv", "pos_kp40_ki40_kd50_PRO7.csv", "pos_kp80_ki40_kd50_PRO8.csv", "pos_kp120_ki40_kd50_PRO9.csv", "pos_kp160_ki40_kd50_PRO10.csv"];
 % file_list = ["pos_kp20_ki40_kd50_PRO1.csv", "pos_kp40_ki40_kd50_PRO2.csv", "pos_kp60_ki40_kd50_PRO3.csv", "pos_kp80_ki40_kd50_PRO4.csv", "pos_kp100_ki40_kd50_PRO5.csv"...
 %                 "pos_kp120_ki40_kd50_PRO6.csv", "pos_kp140_ki40_kd50_PRO7.csv", "pos_kp160_ki40_kd50_PRO8.csv"];
-file_list = ["pos_kp20_ki40_kd50_PRO1.csv", "pos_kp60_ki40_kd50_PRO3.csv", "pos_kp100_ki40_kd50_PRO5.csv"...
-                "pos_kp140_ki40_kd50_PRO7.csv"];
-freqset = [0.2,  0.2*sqrt(2), 0.4, 0.4*sqrt(2), 0.8, 0.8*sqrt(2), 1.6, 1.6*sqrt(2), 3.2, 3.2*sqrt(2), 6.4, 6.4*sqrt(2), 12.8, 12.8*sqrt(2)];
+% file_list = ["pos_kp20_ki40_kd50_PRO1.csv", "pos_kp60_ki40_kd50_PRO3.csv", "pos_kp100_ki40_kd50_PRO5.csv"...
+%                 "pos_kp140_ki40_kd50_PRO7.csv"];
+file_list = ["cur_kp20_kd20_PRO1.csv", "pos_kp40_kd20_PRO2.csv", "pos_kp60_kd20_PRO3.csv"];
+freqset = [0.2,  0.2*sqrt(2), 0.4, 0.4*sqrt(2), 0.8, 0.8*sqrt(2), 1.6, 1.6*sqrt(2), 3.2, 3.2*sqrt(2), 6.4, 6.4*sqrt(2), 12.8];
 c_set = ['b', 'g', 'r', 'm'];
 kp_list = ["Kp=20", "Kp=60", "Kp=100", "Kp=140"];
 
@@ -25,21 +26,21 @@ for i_n = 1:length(file_list)
     F = importdata(file_list(i_n), ',', 1);
     time = F.data(:, 2);
     num = F.data(:, 3);
-    input = F.data(:, 5) ./ 600;
-%     cur = F.data(:, 6);
+    input = F.data(:, 5);
+    output = F.data(:, 6);
 %     vel = F.data(:, 7);
-    output = F.data(:, 8);
+%     output = F.data(:, 8);
     
 %     figure
     
     
-    for i = 1:14
+    for i = 1:13
         X0 = find(num == i - 1, 1);
         X = find(num == i, 1);
         bode_data(i).inputAmp = max(input(X0+10 : X - 1));
         bode_data(i).outputAmp = max(output(X0+10 : X - 1));
         
-        if i == 15
+        if i == 13
             bode_data(i).inputAmp = max(input(X0+10 : end));
             bode_data(i).outputAmp = max(output(X0+10 : end));
         end
@@ -52,7 +53,7 @@ for i_n = 1:length(file_list)
     
     plt(i_n) = plot(freqset, list, c_set(i_n), 'DisplayName', kp_list(i_n));
     grid on
-    xlim([0.5 18])
+    % xlim([0.5 18])
     xlabel("Frequency [Hz]")
     ylabel("Magnitude [dB]")
 
